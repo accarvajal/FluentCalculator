39f580fda6e4475fd502a6645d6091ead1732992
Aldemar Cuartas Carvajal <accarvajal_@hotmail.com>
2024-04-15 17:31:14 -0500

    First Upload

---
+++ FluentCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentCalculatorOperations; 
+
+public class FluentCalculator(double initValue)
+{
+    public double Result { get; set; } = initValue;
+
+    public FluentCalculator Add(double value) => ApplyOperation((resultValue) => resultValue += value);
+
+    public FluentCalculator Subtract(double value) => ApplyOperation((resultValue) => resultValue -= value);
+
+    public FluentCalculator Multiply(double value) => ApplyOperation((resultValue) => resultValue *= value);
+
+    public FluentCalculator Divide(double value) => ApplyOperation((resultValue) => resultValue /= value);
+
+    public FluentCalculator Power(double value) => ApplyOperation((resultValue) => Math.Pow(resultValue, value));
+
+    public FluentCalculator ApplyOperation(Func<double, double> operation)
+    {
+        Result = operation(Result);
+        return this;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using FluentCalculatorOperations;
+
+var calculator = new FluentCalculator(10)
+    .Add(5)
+    .Multiply(2)
+    .Subtract(3)
+    .Divide(2)
+    .Subtract(3.5)
+    .Power(2);
+
+Console.WriteLine("Result: " + calculator.Result); // Output: Result: 13.5
